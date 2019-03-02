32a23e0959e008a9c953e0e640f6bae7485b1796
ArslanAmeer <arslan.ameer@hotmail.com>
2019-03-02 21:25:19 +0500

    📃♻👍 Update: Proper Comments Added

--- SaveRichTextToDB/Controllers/HomeController.cs
+++ SaveRichTextToDB/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace SaveRichTextToDB.Controllers
 {
     public class HomeController : Controller
     {
+        // Db Class Object Intiallization
         private db_htmlEntities1 databaseManager = new db_htmlEntities1();
 
         public ActionResult Index()
@@ -18,17 +19,16 @@ namespace SaveRichTextToDB.Controllers
 
             try
             {
-                // Get info list.
-                model.HTMLContentList = this.databaseManager.display_all_HTML_Content().ToList();
+                // Get List of Rich Text From Database and Then Pass onto VIEW.
+                model.HTMLContentList = databaseManager.display_all_HTML_Content().ToList();
             }
             catch (Exception ex)
             {
-                // Info
+                // If Ever Exception Occurs
                 Console.Write(ex);
             }
 
-            // Info.
-            return this.View(model);
+            return View(model);
         }
 
         [HttpPost]
@@ -38,21 +38,24 @@ namespace SaveRichTextToDB.Controllers
         {
             try
             {
+                // Checking If Entered Data in VIEW is Model Valid or Not
                 if (ModelState.IsValid)
                 {
-                    this.databaseManager.Insert_HTML_Content(model.HtmlContent);
+                    // Saving New ETxt Data onto Database
+                    databaseManager.Insert_HTML_Content(model.HtmlContent);
+                    databaseManager.SaveChanges();
 
-                    this.databaseManager.SaveChanges();
-
-                    model.HTMLContentList = this.databaseManager.display_all_HTML_Content().ToList();
+                    // Getting Fresh Updated List From Database Afetr Saving New Data & Setting up other properties
+                    model.HTMLContentList = databaseManager.display_all_HTML_Content().ToList();
                     model.Message = "Information successfully!! saved.";
                     model.IsValid = true;
                 }
                 else
                 {
+                    // Checking If No Data is Entered In Text Area On View or Is Empty Field passed
                     if (string.IsNullOrEmpty(model.HtmlContent))
                     {
-                        // Settings.
+                        // Setting up other properties
                         model.HTMLContentList = new List<display_all_HTML_Content_Result>();
                         model.Message = "Require field can not be emptied";
                         model.IsValid = false;
--- SaveRichTextToDB/Models/HTMLDisplayViewModel.cs
+++ SaveRichTextToDB/Models/HTMLDisplayViewModel.cs
@@ -9,17 +9,26 @@ namespace SaveRichTextToDB.Models
 {
     public class HTMLDisplayViewModel
     {
+
+        // The AllowHtml attribute can be applied to a Model property and it will disable the validation by ASP.Net MVC only for that particular property.
+        // We need to do this because we are going to store Raw Html of directily into this property.
         [AllowHtml]
         [Required]
         [Display(Name = "HTML Content")]
         public string HtmlContent { get; set; }
 
+
+        // This is Just For Status Message to Show it On Client Area.
         [Display(Name = "Message")]
         public string Message { get; set; }
 
+
+        // This is to Manually Set The Validity to Show Text Or Not.
         [Display(Name = "Is Valid")]
         public bool IsValid { get; set; }
 
+
+        // This Property is To Get list of All Rich TExt From Database
         public List<display_all_HTML_Content_Result> HTMLContentList { get; set; }
     }
 }
